66292293b484e0d9da1a7f04e75b7c303e35e816
SeoU <tjdnfkd1225@naver.com>
2025-05-09 14:21:18 +0900

    feat: Create New Project

---
+++ BossOverHere/BossOverHereApp.swift
@@ -0,0 +1,17 @@
+//
+//  BossOverHereApp.swift
+//  BossOverHere
+//
+//  Created by eunseou on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct BossOverHereApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
